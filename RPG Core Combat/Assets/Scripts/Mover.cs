8440b6cce1d65a4998041df3fb58c7b8f6b8a4d7
Rheditia Ferdiansyah <rheditiaf2406@gmail.com>
2022-06-13 15:46:58 +0700

    Part 2 Introduction to Raycasting

--- RPG Core Combat/Assets/Scripts/Mover.cs
+++ RPG Core Combat/Assets/Scripts/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,8 +7,16 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    private void Update()
+    Ray lastRay;
+
+    void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
